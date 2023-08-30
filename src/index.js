418b26397134816e26ba90b2e136270f31b5e281
Movash <126423674+Movash@users.noreply.github.com>
2023-08-30 22:56:43 +0300

    start_work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// const div = React.createElement('div', user);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(div);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
