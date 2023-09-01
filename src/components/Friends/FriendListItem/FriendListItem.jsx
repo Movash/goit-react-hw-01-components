085e007e7581465bac44cb44051cf702f5f332aa
Movash <126423674+Movash@users.noreply.github.com>
2023-09-01 23:33:10 +0300

    fix1

--- src/components/Friends/FriendListItem/FriendListItem.jsx
+++ src/components/Friends/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendItem } from './FriendListItem.styled';
 
-export const FriendListItem = ({ avatar, name, isOnline, id }) => {
+export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <FriendItem isOnline={isOnline} key={id} className="item">
+    <FriendItem isOnline={isOnline} className="item">
       <span className="status" data-is-online={isOnline}></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
